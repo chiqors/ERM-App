4072f540ec431a7801a590a364d07d87fea82c49
chiqors <bandi_nada@live.com>
2020-08-31 11:31:13 +0700

    Fix temp

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,7 +3,7 @@
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 
 <head>
-    <title>{{ config('app.name') }} - {{ $title }}</title>
+    <title>{{ config('app.name') }}</title>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
