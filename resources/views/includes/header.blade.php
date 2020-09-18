e985c881dd394d5948774ce761edc5a13659256c
alwiyahya99 <alwizero@gmail.com>
2020-09-18 17:24:39 +0700

    - fix header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,23 +1,9 @@
 <nav class="navbar header-navbar pcoded-header">
     <div class="navbar-wrapper">
         <div class="navbar-logo">
-            <a class="mobile-menu waves-effect waves-light" id="mobile-collapse" href="#!">
-                <i class="ti-menu"></i>
-            </a>
-            <div class="mobile-search waves-effect waves-light">
-                <div class="header-search">
-                    <div class="main-search morphsearch-search">
-                        <div class="input-group">
-                            <span class="input-group-prepend search-close"><i
-                                    class="ti-close input-group-text"></i></span>
-                            <input type="text" class="form-control" placeholder="Enter Keyword">
-                            <span class="input-group-append search-btn"><i
-                                    class="ti-search input-group-text"></i></span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <a href="#">
+
+
+            <a>
                 Employee Relationship Management
             </a>
             <a class="mobile-options waves-effect waves-light">
